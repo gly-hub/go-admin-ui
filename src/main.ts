1d2a36684087ad3f31480ad1a2f26e28574626b7
fangao <fangao@staruniongame.com>
2023-08-16 18:47:54 +0800

    feat: 封装icon组件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+import 'virtual:svg-icons-register'
+import registerGlobComp from '@/components'
 
-createApp(App).mount('#app')
+createApp(App).use(registerGlobComp).use(ElementPlus).mount('#app')
